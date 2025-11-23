9d3a5bb4924c55bacdc2d87239fb99c55d8a3661
ubaid Ahmad bhat <bhatubaid341@gmail.com>
2025-11-23 16:03:17 +0530

    Added basic Addition program

---
+++ Addition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a ,b;
+    cout<<"please enter two digits";
+    cin>>a>>b;
+    int sum = a+b;
+    cout<<sum;
+
+
+
+
+return 0;
+}
