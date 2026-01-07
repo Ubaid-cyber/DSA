91e9e334aadd0aa3c5dc456ac993e9ef21337adf
Ubaid Ahmad <bhatubaid341@gmail.com>
2026-01-07 20:37:52 +0530

    add program

---
+++ bitwiseOperator.cxx
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  //  int a =10,b=15;
+    //cout << (a&b) << endl;//
+
+    // bitwise  Or only false when both oprends are false 
+   // cout << (a|b);
+
+    //Xor rule suggested that if the bits are smae then ans will be Zero if bits are different Ans will be one 
+
+    int a = 7;
+    int b=7;
+
+    cout<< (a^b);
+
+
+
+
+
+return 0;
+}
